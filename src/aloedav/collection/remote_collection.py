830f677098259ca4c86782a893601b7c55467387
Michael Godfrey <michael@aloecraft.org>
2026-01-14 22:40:29 -0500

    jhgyft

--- src/aloedav/collection/remote_collection.py
+++ src/aloedav/collection/remote_collection.py
@@ -6,9 +6,10 @@ from uuid import uuid4
 from aloedav.exceptions import PreconditionFailed, WebDAVError
 from aloedav.collection import Collection, ComponentSet, Item, CollectionType
 from aloedav.model.m00_constant import CalendarComponents
-from aloedav.client import AloeDAVClient
+from aloedav.client import AloeDAVClient, BaseAloeDAVClient
 
 class RemoteCollection(Collection):
+    client:BaseAloeDAVClient
     href:str
     ctag:str
     synctoken:str
